86be4df18b6fc18b2100be86435ea3a06c0ce23a
henteko <henteko07@gmail.com>
2014-09-11 18:32:22 +0900

    サンプルロジックを追加

--- AndroidLabTeResSampleApp/app/src/main/java/com/henteko07/androidlabteressampleapp/Model/Divination.java
+++ AndroidLabTeResSampleApp/app/src/main/java/com/henteko07/androidlabteressampleapp/Model/Divination.java
@@ -16,7 +16,49 @@ public class Divination implements Serializable {
         this.firstUser = firstUser;
         this.secondUser = secondUser;
 
-        this.result = "めっちゃいいでw";
-        this.compatibility = 100.0;
+        this.compatibility = getCompatibility(this.firstUser, this.secondUser);
+        this.result = getResult(this.compatibility);
+    }
+
+    private String getResult(Double com) {
+        if (com >= 100) {
+            return "めちゃくちゃ相性ええやん!";
+        }else if (com >= 80) {
+            return "かなり相性ええな！";
+        }else if (com >= 60) {
+            return "なかなか相性ええんちゃう？";
+        }else if (com >= 40) {
+            return "うーん、正直微妙やな…";
+        }else if (com >= 20) {
+            return "うん、まぁ、こういうときもあるよ…";
+        }else {
+            return "逆に相性いいで";
+        }
+    }
+
+    private Double getCompatibility(User firstUser, User secondUser) {
+        int nameStringCount = firstUser.name.length() + secondUser.name.length();
+        int bloodSum = getBloodRank(firstUser.blood) + getBloodRank(secondUser.blood);
+
+        return downDigit(nameStringCount + bloodSum);
+    }
+
+    private Double downDigit(int num) {
+        return (num > 100) ? downDigit(num / 10) : num;
+    }
+
+    private int getBloodRank(Blood blood) {
+        switch (blood) {
+            case A:
+                return 15;
+            case B:
+                return 29;
+            case O:
+                return 43;
+            case AB:
+                return 33;
+        }
+
+        return 100;
     }
 }
